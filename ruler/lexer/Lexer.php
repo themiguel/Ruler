200388bf2579cff425d782e559e49af7f510a02d
Miguel Mawyin <mawyin.miguel@gmail.com>
2021-05-12 17:51:54 -0400

    update to the lexer and test

--- ruler/lexer/Lexer.php
+++ ruler/lexer/Lexer.php
@@ -5,11 +5,124 @@
 	use Ruler\Lexer\Tokens\Quotes;
 	use Ruler\Lexer\Xtring\Xtring;
 
+	use Ruler\Lexer\Tokens\Token;
 	use Ruler\Lexer\Tokens\Identifier;
 	use Ruler\Lexer\Tokens\Number;
 	use Ruler\Lexer\Tokens\Symbol;
 
 	class Lexer{
+		/**
+		 * Extracts the identifier from the xtring
+		 * @param Xtring $xtring The lexer string
+		 * @return Identifier
+		 */
+		protected function extractIdentifier(Xtring $xtring){
+			# Create the token string
+			$token = $xtring->current();
+
+			# Loop through the alphanumeric and _ values
+			while( $xtring->regex('/[A-Za-z0-9_]/', true) && $xtring->next() ){
+				# Add the token
+				$token .= $xtring->current();
+			}
+
+			# Return the identifier
+			return new Identifier($token);
+		}
+
+		/**
+		 * Extracts the number from the xtring
+		 * @param Xtring $xtring The lexer string
+		 * @return Number|Token
+		 */
+		protected function extractNumber(Xtring $xtring){
+			# The token
+			$token = '';
+
+			# Check the current character
+			if( $xtring->is('.') ){
+				# Add a zero
+				$token .= '0';
+			}
+
+			# Add the current digit or dot
+			$token .= $xtring->current();
+
+			# Loop as long as we have digits
+			while( ($xtring->isDigit(true) || $xtring->is('.', true)) && $xtring->next() ){
+				# Add the token
+				$token .= $xtring->current();
+			}
+
+			# Check the token
+			if( is_numeric($token) === false ){
+				# Return a token with the value
+				return new Token($token);
+			}
+
+			# Return the number
+			return new Number($token);
+		}
+
+		/**
+		 * Extracts the quotes from the xtring
+		 * @param Xtring $xtring The lexer string
+		 * @return Quotes|Token
+		 */
+		protected function extractQuotes(Xtring $xtring){
+			# The current token
+			$token = '';
+
+			# Check the peek character for empty string
+			if( $xtring->is('"', true) ){
+				# Go to the next token
+				$xtring->next();
+
+				# Return the empty quotes
+				return new Quotes('');
+			}
+
+			# String is not empty
+			# Loop until we find the next quote
+			while( $xtring->is('"', true) === false && $xtring->next() ){
+				# Add the token
+				$token .= $xtring->current();
+			}
+
+			# Peek character should be a quote
+			# Check to make make sure it is
+			if( $xtring->is('"', true) === false ){
+				# There was an error, either not ending the string
+				# Return a simple token
+				return new Token('"'.$token);
+			}
+
+			# Go to the next quotes
+			$xtring->next();
+
+			# Return the string token
+			return new Quotes($token);
+		}
+
+		/**
+		 * Extracts the symbol from the xtring
+		 * @param Xtring $xtring The lexer string
+		 * @return Symbol
+		 */
+		protected function extractSymbol(Xtring $xtring){
+			# Add the symbol
+			$token = $xtring->current();
+
+			# Loop as long as we have symbols
+			while( $xtring->isSymbol(true) && $xtring->next() ){
+				# Add the token
+				$token .= $xtring->current();
+			}
+
+			# Return the symbol
+			return new Symbol($token);
+		}
+
 		/**
 		 * Tokenize a given rule
 		 * Return a list of token
@@ -28,94 +141,21 @@
 			# Loop through the xtring
 			do{
 				# Check the current character
-				if( $xtring->isAlphabet() ){
-					# Add the character
-					$token = $xtring->current();
-
-					# Loop through the alphanumeric and _ values
-					while( $xtring->regex('/[A-Za-z0-9_]/', true) && $xtring->next() ){
-						# Add the token
-						$token .= $xtring->current();
-					}
-
-					# Create the token
-					$tokens[] = new Identifier($token);
+				if( $xtring->regex('/[A-Za-z_]/') ){
+					# Extract the token
+					$tokens[] = $this->extractIdentifier($xtring);
 				}
 				else if( $xtring->isSymbol() ){
-					# Add the symbol
-					$token = $xtring->current();
-
-					# Loop as long as we have symbols
-					while( $xtring->isSymbol(true) && $xtring->next() ){
-						# Add the token
-						$token .= $xtring->current();
-					}
-
-					# Create the token
-					$tokens[] = new Symbol($token);
+					# Extract the symbol
+					$tokens[] = $this->extractSymbol($xtring);
 				}
 				else if( $xtring->isDigit() || $xtring->is('.') ){
-					# The token
-					$token = '';
-
-					# Check the current character
-					if( $xtring->is('.') ){
-						# Add a zero
-						$token .= '0';
-					}
-
-					# Add the current digit or dot
-					$token .= $xtring->current();
-
-					# Loop as long as we have digits
-					while( ($xtring->isDigit(true) || $xtring->is('.', true)) && $xtring->next() ){
-						# Add the token
-						$token .= $xtring->current();
-					}
-
-					# Check the token
-					if( is_numeric($token) === false ){
-						# Invalid float
-						throw new Exception('Lexer: invalid numeric value `'.$token.'`');
-					}
-
-					# Create the token
-					$tokens[] = new Number($token);
+					# Extract the number
+					$tokens[] = $this->extractNumber($xtring);
 				}
 				else if( $xtring->is('"') ){
-					# The current token
-					$token = '';
-
-					# Check the peek character for empty string
-					if( $xtring->is('"', true) ){
-						# Got an empty quotes
-						# Add an empty quotes
-						$tokens[] = new Quotes('');
-
-						# Go to the next token
-						$xtring->next();
-					}
-					else{
-						# String is not empty
-						# Loop until we find the next quote
-						while( $xtring->is('"', true) === false && $xtring->next() ){
-							# Add the token
-							$token .= $xtring->current();
-						}
-
-						# Peek character should be a quote
-						# Check to make make sure it is
-						if( $xtring->is('"', true) === false ){
-							# There was an error, either not ending the string
-							throw new Exception('Lexer: Missing closing quotes.');
-						}
-
-						# Go to the next quotes
-						$xtring->next();
-
-						# Create the string token
-						$tokens[] = new Quotes($token);
-					}
+					# Extract the quote
+					$tokens[] = $this->extractQuotes($xtring);
 				}
 			} while( $xtring->next() );
 
--- ruler/tests/LexerTest.php
+++ ruler/tests/LexerTest.php
@@ -4,6 +4,7 @@
 	use PHPUnit\Framework\TestCase;
 
 	use Ruler\Lexer\Lexer;
+	use Ruler\Lexer\Tokens\Token;
 	use Ruler\Lexer\Tokens\Number;
 	use Ruler\Lexer\Tokens\Quotes;
 	use Ruler\Lexer\Tokens\Symbol;
@@ -16,57 +17,12 @@
 		 */
 		private $lexer;
 
-		public function setUp():void{
+		public function setUp(): void{
 			# Create a grammar
 			$this->lexer = new Lexer();
 		}
 
-		public function testSimpleNumber():void{
-			$tokens = $this->lexer->tokenize('123');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Number::class, $tokens[0]);
-			$this->assertEquals(123, $tokens[0]->getValue());
-		}
-
-		public function testFloatNumbers():void{
-			$tokens = $this->lexer->tokenize('3.141592');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Number::class, $tokens[0]);
-			$this->assertEquals(3.141592, $tokens[0]->getValue());
-		}
-
-		public function testSimpleIdentifier(): void{
-			$tokens = $this->lexer->tokenize('x');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Identifier::class, $tokens[0]);
-			$this->assertEquals('x', $tokens[0]->getValue());
-		}
-
-		public function testSimpleSymbol():void{
-			$tokens = $this->lexer->tokenize('>=');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Symbol::class, $tokens[0]);
-		}
-
-		public function testEmptyQuotes():void{
-			$tokens = $this->lexer->tokenize('""');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Quotes::class, $tokens[0]);
-		}
-
-		public function testSimpleQuotes():void{
-			$tokens = $this->lexer->tokenize('"There is something in here"');
-			$this->assertEquals(1, count($tokens));
-			$this->assertInstanceOf(Quotes::class, $tokens[0]);
-			$this->assertEquals('There is something in here', $tokens[0]->getValue());
-		}
-
-		public function testInvalidNumber():void{
-			$this->expectException(\Exception::class);
-			$this->lexer->tokenize('1.2.3.4');
-		}
-
-		public function testConditionalRule1():void{
+		public function testConditionalRule1(): void{
 			$tokens = $this->lexer->tokenize('3 < 4');
 			$this->assertEquals(3, count($tokens));
 			$this->assertInstanceOf(Number::class, $tokens[0]);
@@ -77,7 +33,20 @@
 			$this->assertEquals(4, $tokens[2]->getValue());
 		}
 
-		public function testFuncRule1():void{
+		public function testEmptyQuotes(): void{
+			$tokens = $this->lexer->tokenize('""');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Quotes::class, $tokens[0]);
+		}
+
+		public function testFloatNumbers(): void{
+			$tokens = $this->lexer->tokenize('3.141592');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Number::class, $tokens[0]);
+			$this->assertEquals(3.141592, $tokens[0]->getValue());
+		}
+
+		public function testFuncRule1(): void{
 			$tokens = $this->lexer->tokenize('module:func()');
 			$this->assertEquals(4, count($tokens));
 			$this->assertInstanceOf(Identifier::class, $tokens[0]);
@@ -90,7 +59,7 @@
 			$this->assertEquals('()', $tokens[3]->getValue());
 		}
 
-		public function testFuncRule2():void{
+		public function testFuncRule2(): void{
 			$tokens = $this->lexer->tokenize('module:func(1)');
 			$this->assertEquals(6, count($tokens));
 			$this->assertInstanceOf(Identifier::class, $tokens[0]);
@@ -107,7 +76,27 @@
 			$this->assertEquals(')', $tokens[5]->getValue());
 		}
 
-		public function testSimpleExpression():void{
+		public function testIdentifiers(): void{
+			$tokens = $this->lexer->tokenize('one TWO _one_TWO');
+			$this->assertEquals(3, count($tokens));
+			$this->assertInstanceOf(Identifier::class, $tokens[0]);
+			$this->assertInstanceOf(Identifier::class, $tokens[1]);
+			$this->assertInstanceOf(Identifier::class, $tokens[2]);
+			$this->assertEquals('one', $tokens[0]->getValue());
+			$this->assertEquals('TWO', $tokens[1]->getValue());
+			$this->assertEquals('_one_TWO', $tokens[2]->getValue());
+		}
+
+		public function testSimpleTokens(): void{
+			$tokens = $this->lexer->tokenize('1.2.3.4 "token');
+			$this->assertEquals(2, count($tokens));
+			$this->assertInstanceOf(Token::class, $tokens[0]);
+			$this->assertInstanceOf(Token::class, $tokens[1]);
+			$this->assertEquals('1.2.3.4', $tokens[0]->getValue());
+			$this->assertEquals('"token', $tokens[1]->getValue());
+		}
+
+		public function testSimpleExpression(): void{
 			$tokens = $this->lexer->tokenize('1 + 2');
 			$this->assertEquals(3, count($tokens));
 			$this->assertInstanceOf(Number::class, $tokens[0]);
@@ -116,5 +105,32 @@
 			$this->assertEquals(1, $tokens[0]->getValue());
 			$this->assertEquals('+', $tokens[1]->getValue());
 			$this->assertEquals(2, $tokens[2]->getValue());
+		}
+
+		public function testSimpleIdentifier(): void{
+			$tokens = $this->lexer->tokenize('x');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Identifier::class, $tokens[0]);
+			$this->assertEquals('x', $tokens[0]->getValue());
+		}
+
+		public function testSimpleNumber(): void{
+			$tokens = $this->lexer->tokenize('123');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Number::class, $tokens[0]);
+			$this->assertEquals(123, $tokens[0]->getValue());
+		}
+
+		public function testSimpleQuotes(): void{
+			$tokens = $this->lexer->tokenize('"There is something in here"');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Quotes::class, $tokens[0]);
+			$this->assertEquals('There is something in here', $tokens[0]->getValue());
+		}
+
+		public function testSimpleSymbol(): void{
+			$tokens = $this->lexer->tokenize('>=');
+			$this->assertEquals(1, count($tokens));
+			$this->assertInstanceOf(Symbol::class, $tokens[0]);
 		}
 	}
